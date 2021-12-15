2f669a5c9b7a8fd86dd382bfef097e2bb018b841
Evaldas Kazlauskis <evaldas.kazlauskis@eneba.com>
2021-12-15 12:19:19 +0200

    pretify(day)15)

--- advent2021/day_15.py
+++ advent2021/day_15.py
@@ -1,6 +1,9 @@
 from typing import List, Tuple, Generator
 
 
+COORDS = Tuple[int, int]
+
+
 def parse(data: List[str]) -> List[List[int]]:
     return [[int(x) for x in r.strip()] for r in data]
 
@@ -15,47 +18,36 @@ class Map:
         self.lim_x = len(self.data) * self.scale
         self.lim_y = len(self.data[0]) * self.scale
 
-    def neighbours(self, x: int, y: int) -> Generator[Tuple[int, int], None, None]:
-        for i in [-1, 1]:
-            if 0 <= x + i < self.lim_x:
-                yield x + i, y
-            if 0 <= y + i < self.lim_y:
-                yield x, y + i
+    def neighbours(self, x: int, y: int) -> Generator[COORDS, None, None]:
+        for xx, yy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            if 0 <= x + xx < self.lim_x and 0 <= y + yy < self.lim_y:
+                yield x + xx, y + yy
 
-    def finish(self, x: int, y: int):
-        return x == self.lim_y - 1 and y == self.lim_y - 1
+    def finish(self) -> COORDS:
+        return self.lim_y - 1, self.lim_y - 1
 
-    def start(self) -> Tuple[int, int]:
-        return (0, 0)
-
-    def destination(self) -> Tuple[int, int]:
-        return (self.lim_y - 1, self.lim_y - 1)
+    def start(self) -> COORDS:
+        return 0, 0
 
     def cost(self, x: int, y: int) -> int:
         add = x // self.bound_x + y // self.bound_y
         coord_x = x % self.bound_x
         coord_y = y % self.bound_y
-
-        value = self.data[coord_x][coord_y] + add
-        if value > 9:
-            value -= 9
-
-        if value > 9:
-            print(coord_x, coord_y, value)
+        value = (self.data[coord_x][coord_y] + add - 1) % 9 + 1
 
         return value
 
 
 def navigate(map: Map) -> int:
     paths = {}
-    costs = {map.start(): map.cost(*map.start())}
+    costs = {map.start(): 0}
     to_visit = [map.start()]
 
     while to_visit:
         pos = min(to_visit, key=lambda x: costs.get(x, 1_000_000_000))
         to_visit.remove(pos)
 
-        if map.finish(*pos):
+        if pos == map.finish():
             break
 
         for npos in map.neighbours(*pos):
@@ -69,7 +61,7 @@ def navigate(map: Map) -> int:
             if npos not in to_visit:
                 to_visit.append(npos)
 
-    return costs[map.destination()] - costs[map.start()]
+    return costs[map.finish()]
 
 
 if __name__ == "__main__":
